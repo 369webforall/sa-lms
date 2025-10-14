4ed2295e20c240e0ddb3e8b5f8ebbc5330e0719e
369webforall <369webforall@gmail.com>
2025-10-14 17:39:49 +0545

    setup shadcn

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const LoginPage = () => {
+  return (
+    <div>
+      LoginPage <Button>Add Button</Button>
+    </div>
+  );
+};
+
+export default LoginPage;
